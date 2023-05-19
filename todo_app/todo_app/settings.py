66ff9e0dff654557a0b82f04c6966ce0c251614d
PavloShutz <pavlo050706@gmail.com>
2023-05-19 20:53:54 +0300

    security upgrading

--- todo_app/todo_app/settings.py
+++ todo_app/todo_app/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'todo_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'todoapp',
-        'USER': 'postgres',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'NAME': os.getenv("DATABASE_NAME"),
+        'USER': os.getenv("DATABASE_USER"),
+        'PASSWORD': os.getenv("DATABASE_PASSWORD"),
+        'HOST': os.getenv("DATABASE_HOST"),
+        'PORT': os.getenv("DATABASE_PORT"),
     }
 }
 
